0a7bb8c48c9a8257afb5021259f468fc25f70441
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-23 14:00:03 +0530

    Api keys

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello World </h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const APP_ID = "494d6cad";
+  const APP_KEY = "8b3c2862349fd8d9e98d855a920ad00d";
+
+  const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return (
+    <div className="App">
+      <form>
+        <input type="text" />
+        <button type="submit"> </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
