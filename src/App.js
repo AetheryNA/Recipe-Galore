752ba47ae5f5dd6fa146d1308c61082aaf443f02
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-23 17:25:16 +0530

    experimenting with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -7,12 +7,22 @@ const App = () => {
 
   const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect is running.");
+  });
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">
-          Search
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="search-button"
+          type="submit"
+        >
+          {counter}
         </button>
       </form>
     </div>
